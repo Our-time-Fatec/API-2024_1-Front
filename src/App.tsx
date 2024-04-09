36fdee0c9bda56a7aefccf3a8e889acb8046c583
LucasRbnc <lucas.robertonascimento01@gmail.com>
2024-04-08 22:28:25 -0300

    Correção do formato dos documentos de js para tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header/header';
+import Footer from './components/Footer/footer';
+import Home from './routes/Home/home';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Teste</h1>
+    </div>
+  );
+}
