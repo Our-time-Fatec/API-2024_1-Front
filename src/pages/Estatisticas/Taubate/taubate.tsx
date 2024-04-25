9a33515d191e6319a10c0ce56fb12af3a31fb304
andreluke <deco99885@gmail.com>
2024-04-24 22:03:01 -0300

    Edição da página de projetos: Criação das páginas individuais onde serão posts as estatísticas e customização da posição das imagens

---
+++ src/pages/Estatisticas/Taubate/taubate.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './taubate.css'
+
+function Taubate() {
+  return (
+    <div id='page-container'>
+      <div id='text-box'>
+      <h1>Bem-vindo à nossa página de estatísticas sobre Taubate!</h1>
+      <p>Aqui você encontrará informações sobre elas.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Taubate;
--- src/pages/Projetos/projetos.tsx
+++ src/pages/Projetos/projetos.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 import "./projetos.css"
 
 function projetos(): JSX.Element {
@@ -7,20 +8,28 @@ function projetos(): JSX.Element {
 
   return (
     <div id='page-container'>
-      <div className='container' id="atibaia">
+      <div className='photo-container'>
+      <div className='container geral' id="atibaia">
+        <Link to={'/atibaia'}>
       <Card className="imagem_demonstrativa">
       <Card.Img src={require('../../assets/atibaia_imagem.jpg')} alt="Card image" />
     </Card>
+    </Link>
       </div>
-      <div className='container' id="cruzeiro">
+      <div className='container geral' id="cruzeiro">
+      <Link to={'/cruzeiro'}>
       <Card className="imagem_demonstrativa">
       <Card.Img src={require('../../assets/cruzeiro_imagem.jpg')} alt="Card image" />
     </Card>
+    </Link>
       </div>
-      <div className='container' id="taubate">
+      <div className='container geral' id="taubate">
+      <Link to={'/taubate'}>
       <Card className="imagem_demonstrativa">
       <Card.Img src={require('../../assets/taubate_imagem.jpg')} alt="Card image" />
     </Card>
+    </Link>
+      </div>
       </div>
     </div>
   );
