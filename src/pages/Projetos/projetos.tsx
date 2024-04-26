fc1f5d2904d65dfc716ca2b1c48ed55079973528
andreluke <deco99885@gmail.com>
2024-04-25 22:34:12 -0300

    Adição dos cards nas páginas de introdução aos projetos

--- src/pages/Projetos/projetos.tsx
+++ src/pages/Projetos/projetos.tsx
@@ -9,26 +9,57 @@ function projetos(): JSX.Element {
   return (
     <div id='page-container'>
       <div className='photo-container'>
+
       <div className='container geral' id="atibaia">
+        <div className='border'>
         <Link to={'/atibaia'}>
-      <Card className="imagem_demonstrativa">
-      <Card.Img src={require('../../assets/atibaia_imagem.jpg')} alt="Card image" />
+        <Card >
+      <Card.Img variant="top" src={require('../../assets/atibaia_imagem.jpg')} className='imagem_demonstrativa' alt="Card image" />
+      <Card.Body className="conteudo-com-borda">
+        <Card.Title>Meu nome</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        
+      </Card.Body>
     </Card>
+
     </Link>
+    </div>
       </div>
       <div className='container geral' id="cruzeiro">
+        <div className='border'>
       <Link to={'/cruzeiro'}>
-      <Card className="imagem_demonstrativa">
-      <Card.Img src={require('../../assets/cruzeiro_imagem.jpg')} alt="Card image" />
+      <Card >
+      <Card.Img variant="top" src={require('../../assets/cruzeiro_imagem.jpg')} className='imagem_demonstrativa' alt="Card image" />
+      <Card.Body className="conteudo-com-borda">
+        <Card.Title>Meu nome</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
     </Card>
     </Link>
+    </div>
       </div>
       <div className='container geral' id="taubate">
+        <div className='border'>
       <Link to={'/taubate'}>
-      <Card className="imagem_demonstrativa">
-      <Card.Img src={require('../../assets/taubate_imagem.jpg')} alt="Card image" />
+      <Card >
+      <Card.Img variant="top" src={require('../../assets/taubate_imagem.jpg')} className='imagem_demonstrativa' alt="Card image" />
+      <Card.Body className="conteudo-com-borda">
+        <Card.Title>Meu nome</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        
+      </Card.Body>
     </Card>
     </Link>
+    </div>
       </div>
       </div>
     </div>
