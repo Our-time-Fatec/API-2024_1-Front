e8c0727b7079f49c71ed9872b68fa38019ce25b1
andreluke <deco99885@gmail.com>
2024-05-15 22:14:12 -0300

    Modelo de estatistica inserido

--- src/pages/Estatisticas/Atibaia/atibaia.tsx
+++ src/pages/Estatisticas/Atibaia/atibaia.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ProgressBar from 'react-bootstrap/ProgressBar';
+import { PieChart } from '@mui/x-charts/PieChart';
+import Box from '@mui/material/Box';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './atibaia.css'
 
@@ -56,6 +58,22 @@ function Atibaia() {
         <Progress />
       </div>
         <div className="map-container">
+        <div id="statistics">
+          
+      <PieChart
+      series={[
+        {
+          data: [
+            { id: 0, value: 478, label: 'Quantidade de Solo exposto' },
+            { id: 1, value: 112, label: 'Quantidade de Nova Edificação' },
+            { id: 2, value: 371, label: 'Quantidade de Supressão de Vegetação' },
+          ],
+        },
+      ]}
+      width={200}
+      height={200}
+    />
+      </div>
         <div className="square"><a>Mapa/Gráfico</a></div>
       </div>
     </div>
--- src/pages/Estatisticas/Cruzeiro/cruzeiro.tsx
+++ src/pages/Estatisticas/Cruzeiro/cruzeiro.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './cruzeiro.css'
+import { PieChart } from '@mui/x-charts/PieChart';
+
+
 
 function Cruzeiro() {
   return (
@@ -7,6 +10,21 @@ function Cruzeiro() {
       <div id='text-box'>
       <h1>Bem-vindo à nossa página de estatísticas sobre Cruzeiro!</h1>
       <p>Aqui você encontrará informações sobre elas.</p>
+      </div>
+      <div id="statistics">
+      <PieChart
+      series={[
+        {
+          data: [
+            { id: 0, value: 10, label: 'series A' },
+            { id: 1, value: 15, label: 'series B' },
+            { id: 2, value: 20, label: 'series C' },
+          ],
+        },
+      ]}
+      width={400}
+      height={200}
+    />
       </div>
     </div>
   );
